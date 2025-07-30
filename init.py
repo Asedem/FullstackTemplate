04a2e685af8a1cba43e3dcb8dbcc6fc0390ba22e
Asedem <asedem.games@gmail.com>
2025-07-30 12:58:35 +0200

    Replacement in frontend and docker-compose

--- init.py
+++ init.py
@@ -23,6 +23,11 @@ def replace_in_file(file_path, old, new, count = -1):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+
+def replace_with_message(project_part, file_path, old, new):
+    print(f'Replacing names in {project_part} ({file_path})')
+    replace_in_file(f'.{project_part}/{file_path}', old, new)
+
 # Main Logic
 
 proj_name = read_string('Enter a name for the Project: ')
@@ -35,5 +40,11 @@ db_name = proj_id
 if not read_yesno('Would you like to use the project id as your database name?'):
     db_name = read_string('Enter a name for your database: ')
 
-print('Replacing names in database (create_database.sql)')
-replace_in_file('./database/create_database.sql', 'x', db_name)
+comp_file = 'docker-compose.yml'
+
+replace_with_message('/database', 'create_database.sql', 'x', db_name)
+replace_with_message('/frontend', 'index.html', 'Fullstack template', proj_name)
+replace_with_message('/', comp_file, 'x_backend', f'{proj_id}_backend')
+replace_with_message('/', comp_file, 'x_frontend', f'{proj_id}_frontend')
+replace_with_message('/', comp_file, 'x_database', f'{proj_id}_database')
+replace_with_message('/', comp_file, 'x_internal_network', f'{proj_id}_internal_network')
